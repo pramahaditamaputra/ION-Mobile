67c83389d3e4f49dc3671ae680d379baabf40554
PramahadiTamaPutra <pramahaditamaputra@outlook.com>
2021-04-23 12:45:39 +0700

    AddedReleaseAndroid

--- android/app/src/main/java/com/ion/MainActivity.java
+++ android/app/src/main/java/com/ion/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ion;
+package com.ion.walkin.juaracoding;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ION";
+    return "WalkIn";
   }
 }
